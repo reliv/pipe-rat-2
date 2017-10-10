bdfe99375cec9bb93f1d606b7ad60725ba9288af
James Jervis <jjervis@relivinc.com>
2017-10-10 14:56:59 -0500

    Add where mutator

---
+++ request-attribute/src/Http/RequestAttributeWhereMutator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Reliv\PipeRat2\RequestAttribute\Http;
+
+/**
+ * @author James Jervis - https://github.com/jerv13
+ */
+interface RequestAttributeWhereMutator
+{
+    const ATTRIBUTE = RequestAttributeWhere::ATTRIBUTE;
+}

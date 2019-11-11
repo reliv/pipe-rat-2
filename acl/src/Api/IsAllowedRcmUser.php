6ee5b8b765b16bb8239bee92100f9d8a6286a80c
Rod McNew <rmcnew@relivinc.com>
2019-11-11 12:38:10 -0600

    Temp fix pipe rat 2 admin API functionality.

--- acl/src/Api/IsAllowedRcmUser.php
+++ acl/src/Api/IsAllowedRcmUser.php
@@ -2,20 +2,32 @@
 
 namespace Reliv\PipeRat2\Acl\Api;
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Rcm\Acl\AclActions;
 use Reliv\PipeRat2\Options\Options;
 
 /**
- * @author James Jervis - https://github.com/jerv13
+ * @deprecated This will be removed eventully. Use the new ACL system instead.
+ *
+ * Class IsAllowedRcmUser
+ * @package Reliv\PipeRat2\Acl\Api
  */
 class IsAllowedRcmUser implements IsAllowed
 {
     const OPTION_RESOURCE_ID = 'resourceId';
     const OPTION_PRIVILEGE = 'privilege';
+    protected $requestContext;
+
+    public function __construct(ContainerInterface $requestContext)
+    {
+        $this->requestContext = $requestContext;
+    }
 
     /**
+     * @deprecated This will be removed eventully. Use the new ACL system instead.
      * @param ServerRequestInterface $request
-     * @param array                  $options
+     * @param array $options
      *
      * @return bool
      * @throws \Exception
@@ -24,6 +36,18 @@ class IsAllowedRcmUser implements IsAllowed
         ServerRequestInterface $request,
         array $options = []
     ): bool {
-        return false; //This was disabled in the ACL 2 project because it didn't follow new ACL rules.
+        /**
+         * @var AssertIsAllowed $assertIsAllowed
+         */
+        $assertIsAllowed = $this->requestContext->get(AssertIsAllowed::class);
+
+        try {
+            //Note that "legacy-global-admin-functionality" is temporary and will be removed eventually.
+            $assertIsAllowed->__invoke(AclActions::EXECUTE, ['type' => 'legacy-global-admin-functionality']);
+
+            return true;
+        } catch (NotAllowedException $e) {
+            return false;
+        }
     }
 }
